8f778ae68ed452fd049380c59386dec768a2d886
Cyril CHAPON <cyrilchapon@users.noreply.github.com>
2021-09-02 14:04:59 +0200

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 const sum = (...ns: number[]) => ns.reduce((acc, n) => acc + n, 0)
 
+// Non mutating array replace like ns[index] = fn()
 const replaceAtIndex = <T>(fn: (input: T) => T) => (ns: T[], index: number): T[] => ([
   ...ns.slice(0, index),
   fn(ns[index]),
